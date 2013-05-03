bccd86152d4c5e34a6330ac3bda50422b8939735
Nuno Esteves <nuno.v.esteves@gmail.com>
2013-05-03 18:28:24 +0100

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  # Includes the Capybara domain-specific language (DSL) in the RSpec helper file
+  config.include Capybara::DSL
 end
