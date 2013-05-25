51c8d6948ae0b923c39960556cf8536c62cb86a4
Nuno Esteves <nuno.v.esteves@gmail.com>
2013-05-25 21:48:16 +0100

    Finish basic sign-in-out functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	has_secure_password
 	before_save { self.email = email.downcase }
+	before_save :create_remember_token
 
 	validates :name, 	presence: true,
 										length: { maximum: 50 }
@@ -9,6 +9,13 @@ class User < ActiveRecord::Base
   									format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 	
+	has_secure_password
 	validates :password_confirmation, presence: true
 	validates :password, length: { minimum: 6 }
+
+	private
+
+		def create_remember_token
+			self.remember_token = SecureRandom.urlsafe_base64
+		end
 end
